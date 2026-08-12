88d9fcf33b28d065f43f0e1f008e689ffbebb892
Chuz777 <72413524+Chuz777@users.noreply.github.com>
2026-08-11 23:16:38 -0600

    Cambios
    Botones y demás

---
+++ Proyecto/Controllers/InventarioController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Proyecto.Filters;
+
+namespace Proyecto.Controllers
+{
+    public class InventarioController : BaseController
+    {
+        // GET: Inventario
+        public ActionResult Index()
+        {
+            return View("Inventario");
+        }
+
+
+        }
+    }
+
