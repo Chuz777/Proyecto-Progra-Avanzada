f474f437cfe93d99c332c511af11543769bd04a0
NuchDev <sofiununez@gmail.com>
2026-08-13 17:06:36 -0600

    cambios visuales-roles
    Cambios visuales ocultando botones dependiendo de los roles

--- Proyecto/Controllers/BaseController.cs
+++ Proyecto/Controllers/BaseController.cs
@@ -7,11 +7,16 @@ namespace Proyecto.Controllers
         protected string RolActual => Session["Rol"] as string;
         protected string UsuarioActual => Session["Username"] as string;
 
+        protected bool EsAdmin => RolActual == "Admin";
+        protected bool EsVendedor => RolActual == "Admin" || RolActual == "Asesor"; 
+
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             base.OnActionExecuting(filterContext);
             ViewBag.UsuarioActual = UsuarioActual;
             ViewBag.RolActual = RolActual;
+            ViewBag.EsAdmin = EsAdmin;
+            ViewBag.EsVendedor = EsVendedor;
         }
     }
 }
